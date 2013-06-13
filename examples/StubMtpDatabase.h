8c8f60fe4daf90cbfee8cf053cea46cdf1538acd
Thomas Voß <thomas.voss@canonical.com>
2013-06-13 12:22:21 +0200

    Initial push.

---
+++ examples/StubMtpDatabase.h
@@ -0,0 +1,354 @@
+#ifndef STUB_MTP_DATABASE_H_
+#define STUB_MTP_DATABASE_H_
+
+#include <mtp.h>
+#include <MtpDatabase.h>
+#include <MtpDataPacket.h>
+#include <MtpObjectInfo.h>
+#include <MtpProperty.h>
+
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <string>
+#include <tuple>
+
+namespace android
+{
+class StubMtpDatabase : public android::MtpDatabase {
+private:
+    struct DbEntry
+    {
+        MtpStorageID storage_id;
+        std::string object_name;
+        MtpObjectFormat object_format;
+        size_t object_size;
+        size_t width;
+        size_t height;
+        size_t bit_depth;
+        std::string display_name;
+        std::string path;
+    };
+
+    enum PngImageHandles
+    {
+        png_image_1,
+        png_image_2,
+        png_image_3,
+        png_image_4,
+        png_image_5,
+        png_image_6,                        
+        png_image_7,
+        png_image_8,
+        png_image_9,
+        png_image_10,
+        png_image_size                        
+    };
+    static const uint32_t reserved_slots = png_image_size;
+    
+    uint32_t counter;
+    std::map<std::tuple<MtpStorageID, MtpObjectFormat>, MtpObjectHandleList> db = 
+    {
+        {
+            std::make_tuple(MTP_STORAGE_REMOVABLE_RAM, MTP_FORMAT_PNG), 
+            {png_image_1, png_image_2, png_image_3, png_image_4, png_image_5, png_image_6, png_image_7, png_image_8, png_image_9, png_image_10}
+        }
+    };
+    
+    std::map<MtpObjectHandle, DbEntry> entries = 
+    {
+        {png_image_1, {MTP_STORAGE_REMOVABLE_RAM, "png_image_1", MTP_FORMAT_PNG, 20 * 20 * 4, 20, 20, 4, "png_image_1", "png_image_1.png"}},
+        {png_image_2, {MTP_STORAGE_REMOVABLE_RAM, "png_image_2", MTP_FORMAT_PNG, 20 * 20 * 4, 20, 20, 4, "png_image_2", "png_image_2.png"}},
+        {png_image_3, {MTP_STORAGE_REMOVABLE_RAM, "png_image_3", MTP_FORMAT_PNG, 20 * 20 * 4, 20, 20, 4, "png_image_3", "png_image_3.png"}},
+        {png_image_4, {MTP_STORAGE_REMOVABLE_RAM, "png_image_4", MTP_FORMAT_PNG, 20 * 20 * 4, 20, 20, 4, "png_image_4", "png_image_4.png"}},                
+        {png_image_5, {MTP_STORAGE_REMOVABLE_RAM, "png_image_5", MTP_FORMAT_PNG, 20 * 20 * 4, 20, 20, 4, "png_image_5", "png_image_5.png"}},
+        {png_image_6, {MTP_STORAGE_REMOVABLE_RAM, "png_image_6", MTP_FORMAT_PNG, 20 * 20 * 4, 20, 20, 4, "png_image_6", "png_image_6.png"}},
+        {png_image_7, {MTP_STORAGE_REMOVABLE_RAM, "png_image_7", MTP_FORMAT_PNG, 20 * 20 * 4, 20, 20, 4, "png_image_7", "png_image_7.png"}},
+        {png_image_8, {MTP_STORAGE_REMOVABLE_RAM, "png_image_8", MTP_FORMAT_PNG, 20 * 20 * 4, 20, 20, 4, "png_image_8", "png_image_8.png"}},
+        {png_image_9, {MTP_STORAGE_REMOVABLE_RAM, "png_image_9", MTP_FORMAT_PNG, 20 * 20 * 4, 20, 20, 4, "png_image_9", "png_image_9.png"}},
+        {png_image_10, {MTP_STORAGE_REMOVABLE_RAM, "png_image_10", MTP_FORMAT_PNG, 20 * 20 * 4, 20, 20, 4, "png_image_10", "png_image_10.png"}}
+    };
+    
+public:
+    StubMtpDatabase() : counter(StubMtpDatabase::reserved_slots)
+    {
+    }
+    virtual ~StubMtpDatabase() {}
+
+    // called from SendObjectInfo to reserve a database entry for the incoming file
+    virtual MtpObjectHandle beginSendObject(
+        const MtpString& path,
+        MtpObjectFormat format,
+        MtpObjectHandle parent,
+        MtpStorageID storage,
+        uint64_t size,
+        time_t modified)
+    {
+        std::cout << __PRETTY_FUNCTION__ << ": " << path << std::endl;
+        return counter++; 
+    }
+
+    // called to report success or failure of the SendObject file transfer
+    // success should signal a notification of the new object's creation,
+    // failure should remove the database entry created in beginSendObject
+    virtual void endSendObject(
+        const MtpString& path,
+        MtpObjectHandle handle,
+        MtpObjectFormat format,
+        bool succeeded)
+    {
+        std::cout << __PRETTY_FUNCTION__ << ": " << path << std::endl;
+    }
+
+    virtual MtpObjectHandleList* getObjectList(
+        MtpStorageID storageID,
+        MtpObjectFormat format,
+        MtpObjectHandle parent)
+    {
+        std::cout << __PRETTY_FUNCTION__ << ": " << storageID << ", " << format << ", " << parent << std::endl;
+        MtpObjectHandleList* list = nullptr;
+        try
+        {
+            list = new MtpObjectHandleList(db.at(std::make_tuple(storageID, MTP_FORMAT_PNG)));
+        } catch(...)
+        {
+            list = new MtpObjectHandleList();
+        }
+        
+        return list;
+    }
+
+    virtual int getNumObjects(
+        MtpStorageID storageID,
+        MtpObjectFormat format,
+        MtpObjectHandle parent)
+    {
+        std::cout << __PRETTY_FUNCTION__ << ": " << storageID << ", " << format << ", " << parent << std::endl;
+        try
+        {
+            return db.at(std::make_tuple(storageID, MTP_FORMAT_PNG)).size();
+        } catch(...)
+        {
+        }
+        
+        return 0;
+    }
+
+    // callee should delete[] the results from these
+    // results can be NULL
+    virtual MtpObjectFormatList* getSupportedPlaybackFormats()
+    {
+        std::cout << __PRETTY_FUNCTION__ << std::endl;
+        static const MtpObjectFormatList list = {MTP_FORMAT_PNG};
+        return new MtpObjectFormatList{list};
+    }
+    
+    virtual MtpObjectFormatList* getSupportedCaptureFormats()
+    {
+        std::cout << __PRETTY_FUNCTION__ << std::endl;
+        static const MtpObjectFormatList list = {MTP_FORMAT_ASSOCIATION, MTP_FORMAT_PNG};
+        return new MtpObjectFormatList{list};
+    }
+    
+    virtual MtpObjectPropertyList* getSupportedObjectProperties(MtpObjectFormat format)
+    {
+        std::cout << __PRETTY_FUNCTION__ << std::endl;
+        if (format != MTP_FORMAT_PNG)
+            return nullptr;
+            
+        static const MtpObjectPropertyList list = 
+        {
+            MTP_PROPERTY_STORAGE_ID,
+            MTP_PROPERTY_OBJECT_FORMAT,
+            MTP_PROPERTY_OBJECT_SIZE,
+            MTP_PROPERTY_WIDTH,
+            MTP_PROPERTY_HEIGHT,
+            MTP_PROPERTY_IMAGE_BIT_DEPTH,
+            MTP_PROPERTY_DISPLAY_NAME            
+        };
+         
+        return new MtpObjectPropertyList{list};
+    }
+    
+    virtual MtpDevicePropertyList* getSupportedDeviceProperties()
+    {
+        std::cout << __PRETTY_FUNCTION__ << std::endl;
+        static const MtpDevicePropertyList list = { MTP_DEVICE_PROPERTY_UNDEFINED };
+        return new MtpDevicePropertyList{list};
+    }
+
+    virtual MtpResponseCode getObjectPropertyValue(
+        MtpObjectHandle handle,
+        MtpObjectProperty property,
+        MtpDataPacket& packet)
+    {        
+        std::cout << __PRETTY_FUNCTION__ << std::endl;
+        switch(property)
+        {
+            case MTP_PROPERTY_STORAGE_ID: packet.putUInt32(entries.at(handle).storage_id); break;            
+            case MTP_PROPERTY_OBJECT_FORMAT: packet.putUInt32(entries.at(handle).object_format); break;
+            case MTP_PROPERTY_OBJECT_SIZE: packet.putUInt32(entries.at(handle).object_size); break;
+            case MTP_PROPERTY_WIDTH: packet.putUInt32(entries.at(handle).width); break;
+            case MTP_PROPERTY_HEIGHT: packet.putUInt32(entries.at(handle).height); break;
+            case MTP_PROPERTY_IMAGE_BIT_DEPTH: packet.putUInt32(entries.at(handle).bit_depth); break;
+            case MTP_PROPERTY_DISPLAY_NAME: packet.putString(entries.at(handle).display_name.c_str()); break;
+            default: return MTP_RESPONSE_GENERAL_ERROR; break;                
+        }
+        
+        return MTP_RESPONSE_OK;
+    }
+
+    virtual MtpResponseCode setObjectPropertyValue(
+        MtpObjectHandle handle,
+        MtpObjectProperty property,
+        MtpDataPacket& packet)
+    {
+        std::cout << __PRETTY_FUNCTION__ << std::endl;
+        return MTP_RESPONSE_OPERATION_NOT_SUPPORTED;
+    }
+
+    virtual MtpResponseCode getDevicePropertyValue(
+        MtpDeviceProperty property,
+        MtpDataPacket& packet)
+    {
+        std::cout << __PRETTY_FUNCTION__ << std::endl;
+        return MTP_RESPONSE_GENERAL_ERROR;
+    }
+
+    virtual MtpResponseCode setDevicePropertyValue(
+        MtpDeviceProperty property,
+        MtpDataPacket& packet)
+    {
+        std::cout << __PRETTY_FUNCTION__ << std::endl;
+        return MTP_RESPONSE_OPERATION_NOT_SUPPORTED;
+    }
+
+    virtual MtpResponseCode resetDeviceProperty(
+        MtpDeviceProperty property)
+    {
+        std::cout << __PRETTY_FUNCTION__ << std::endl;
+        return MTP_RESPONSE_OPERATION_NOT_SUPPORTED;
+    }
+
+    virtual MtpResponseCode getObjectPropertyList(
+        MtpObjectHandle handle,
+        uint32_t format, 
+        uint32_t property,
+        int groupCode, 
+        int depth,
+        MtpDataPacket& packet)
+    {
+        std::cout << __PRETTY_FUNCTION__ << std::endl;
+        return MTP_RESPONSE_OPERATION_NOT_SUPPORTED;
+    }
+
+    virtual MtpResponseCode getObjectInfo(
+        MtpObjectHandle handle,
+        MtpObjectInfo& info)
+    {
+        std::cout << __PRETTY_FUNCTION__ << std::endl;
+        info.mHandle = handle;
+        info.mStorageID = entries.at(handle).storage_id;
+        info.mFormat = entries.at(handle).object_format;
+        info.mProtectionStatus = 0x0;
+        info.mCompressedSize = 0;
+        info.mThumbFormat = entries.at(handle).object_format;
+        info.mThumbCompressedSize = 20*20*4;
+        info.mThumbPixWidth = 20;
+        info.mThumbPixHeight  =20;
+        info.mImagePixWidth = 20;
+        info.mImagePixHeight = 20;
+        info.mImagePixDepth = 4;
+        info.mParent = MTP_PARENT_ROOT;
+        info.mAssociationType = 0;
+        info.mAssociationDesc = 0;
+        info.mSequenceNumber = 0;
+        info.mName = ::strdup("test");
+        info.mDateCreated = 0;
+        info.mDateModified = 0;
+        info.mKeywords = ::strdup("ubuntu,touch");
+        
+        return MTP_RESPONSE_OK;
+    }
+
+    virtual void* getThumbnail(MtpObjectHandle handle, size_t& outThumbSize)
+    {
+        std::cout << __PRETTY_FUNCTION__ << std::endl;
+        outThumbSize = 20*20*4;
+        void* result = malloc(20*20*4);
+        memset(result, 0, 20*20*4);
+        return result;
+    }
+
+    virtual MtpResponseCode getObjectFilePath(
+        MtpObjectHandle handle,
+        MtpString& outFilePath,
+        int64_t& outFileLength,
+        MtpObjectFormat& outFormat)
+    {
+        std::cout << __PRETTY_FUNCTION__ << std::endl;
+        outFilePath = entries.at(handle).path;
+        outFileLength = 1024;
+        outFormat = entries.at(handle).object_format;
+    }
+
+    virtual MtpResponseCode deleteFile(MtpObjectHandle handle)
+    {
+        std::cout << __PRETTY_FUNCTION__ << std::endl;
+        return MTP_RESPONSE_OPERATION_NOT_SUPPORTED;
+    }
+
+    virtual MtpObjectHandleList* getObjectReferences(MtpObjectHandle handle)
+    {
+        std::cout << __PRETTY_FUNCTION__ << std::endl;
+        return nullptr;
+    }
+
+    virtual MtpResponseCode setObjectReferences(
+        MtpObjectHandle handle,
+        MtpObjectHandleList* references)
+    {
+        std::cout << __PRETTY_FUNCTION__ << std::endl;
+        return MTP_RESPONSE_OPERATION_NOT_SUPPORTED;    
+    }
+
+    virtual MtpProperty* getObjectPropertyDesc(
+        MtpObjectProperty property,
+        MtpObjectFormat format)
+    {
+        std::cout << __PRETTY_FUNCTION__ << std::endl;
+        MtpProperty* result = nullptr;
+        switch(property)
+        {
+            case MTP_PROPERTY_STORAGE_ID: result = new MtpProperty(property, MTP_TYPE_UINT32); break;
+            case MTP_PROPERTY_OBJECT_FORMAT: result = new MtpProperty(property, MTP_TYPE_UINT32); break;
+            case MTP_PROPERTY_OBJECT_SIZE: result = new MtpProperty(property, MTP_TYPE_UINT32); break;
+            case MTP_PROPERTY_WIDTH: result = new MtpProperty(property, MTP_TYPE_UINT32); break;
+            case MTP_PROPERTY_HEIGHT: result = new MtpProperty(property, MTP_TYPE_UINT32); break;
+            case MTP_PROPERTY_IMAGE_BIT_DEPTH: result = new MtpProperty(property, MTP_TYPE_UINT32); break;
+            case MTP_PROPERTY_DISPLAY_NAME: result = new MtpProperty(property, MTP_TYPE_STR); break;
+            default: break;                
+        }
+        
+        return result;
+    }
+
+    virtual MtpProperty* getDevicePropertyDesc(MtpDeviceProperty property)
+    {
+        std::cout << __PRETTY_FUNCTION__ << std::endl;
+        return new MtpProperty(MTP_DEVICE_PROPERTY_UNDEFINED, MTP_TYPE_UNDEFINED);
+    }
+    
+    virtual void sessionStarted()
+    {
+        std::cout << __PRETTY_FUNCTION__ << std::endl;
+    }
+
+    virtual void sessionEnded()
+    {
+        std::cout << __PRETTY_FUNCTION__ << std::endl;
+    }
+};
+}
+
+#endif // STUB_MTP_DATABASE_H_
---
+++ examples/server.cpp
@@ -0,0 +1,67 @@
+#include "StubMtpDatabase.h"
+
+#include <MtpServer.h>
+#include <MtpStorage.h>
+
+#include <iostream>
+
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+namespace
+{
+struct FileSystemConfig
+{
+    static const int file_group = 1023; // Taken from android's config. Might need to be adjusted.
+    static const int file_perm = 0664;
+    static const int directory_perm = 0755;
+};
+
+android::MtpStorage* removable_storage = new android::MtpStorage(
+    MTP_STORAGE_REMOVABLE_RAM, 
+    ".",
+    "Dummy storage", 
+    1024 * 1024 * 1024 * 200,
+    true,
+    1024 * 1024 * 1024);
+}
+
+int main(int argc, char** argv)
+{
+    int fd = open("/dev/mtp_usb", O_RDWR);
+    
+    if (fd < 0)
+    {
+        std::cout << "Error opening /dev/mtp_usb, aborting now..." << std::endl;
+    }
+        
+    std::shared_ptr<android::MtpServer> server
+    {
+        new android::MtpServer(
+            fd, 
+            new android::StubMtpDatabase(),
+            false, 
+            FileSystemConfig::file_group, 
+            FileSystemConfig::file_perm, 
+            FileSystemConfig::directory_perm)
+    };
+    server->addStorage(removable_storage);
+    server->run();
+
+    /*
+      std::thread t{[&server](){ server->run(); }};
+
+      sigset_t signal_set;
+      sigemptyset(&signal_set);
+      sigaddset(&signal_set, SIGINT);
+      int signal;
+      sigwait(&signal_set, &signal);
+
+      if (t.joinable())
+      t.join();
+    */
+    
+}
