f9023e4f73f4c50fb2d35ab4c41d8a8e50186656
Bhushan Shah <bhush94@gmail.com>
2015-12-18 12:29:12 +0530

    try harder to not interact with unity greeter

--- server/server.cpp
+++ server/server.cpp
@@ -348,7 +348,8 @@ public:
     void run()
     {
         // FIXME: Explicit false to run this on non-greeter environments
-        if (false && is_active->get()) {
+        //if (false && is_active->get()) {
+        if (false) {
             is_active->changed().connect([this](bool active)
             {
                 if (!active) {
